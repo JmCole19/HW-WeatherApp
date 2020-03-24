9a7477798681c2154ee498e7ecae186e8a89850f
Jason Coleman <jayemcee121@gmail.com>
2020-03-24 14:47:06 -0400

    SearchBar functionality and display location name

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-$('.dropdown-trigger').dropdown();
+$(document).ready(function () {
+    var submitSearchBtn = $("#submitSearchBtn");
+    var searchValue;
 
-function openSearch() {
-    document.getElementById("myOverlay").style.display = "block";
-  }
-  
-  function closeSearch() {
-    document.getElementById("myOverlay").style.display = "none";
-  }
+    submitSearchBtn.on("click", function () {
+        searchValue = $(".searchInput").val()
+        changeLocation();
+        console.log(searchValue)
+    }) 
+
+    function changeLocation() {
+        var queryURL = `http://api.openweathermap.org/data/2.5/weather?zip=${searchValue},us&appid=f17120984cd2d82d4bc796031344984c`
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            var displayCityName = $("<h2>").text(response.name)
+            $(".container").append(displayCityName);
+        })
+    }
+    
+
+})
+
